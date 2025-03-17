29c41c9abcffe2249a736ee43ff3d549d7856877
Cees van de Griend <cees.van.de.griend@griend.eu>
2025-03-17 15:28:37 +0100

    feat: switched to daily

--- stocks/utils/db_stock.py
+++ stocks/utils/db_stock.py
@@ -1,5 +1,7 @@
 import psycopg
 
+from sqlalchemy.dialects.postgresql import insert
+
 
 __VERSION__ = "0.0.1"
 
@@ -7,17 +9,17 @@ __stocks_uri: str = None
 
 
 CREATE_STOCKS_TABLE = """
-CREATE TABLE IF NOT EXISTS stocks (
-    datetime     TIMESTAMP WITH TIME ZONE NOT NULL,
-    symbol       VARCHAR(128)             NOT NULL,
-    open         REAL                     NOT NULL,
-    high         REAL                     NOT NULL,
-    low          REAL                     NOT NULL,
-    close        REAL                     NOT NULL,
-    volume       REAL                     NOT NULL,
-    dividends    REAL                     NOT NULL,
-    splits       REAL                     NOT NULL,
-    UNIQUE (datetime, symbol)
+CREATE TABLE IF NOT EXISTS daily (
+    day          DATE         NOT NULL,
+    symbol       VARCHAR(128) NOT NULL,
+    open         REAL         NOT NULL,
+    high         REAL         NOT NULL,
+    low          REAL         NOT NULL,
+    close        REAL         NOT NULL,
+    volume       REAL         NOT NULL,
+    dividends    REAL         NOT NULL,
+    splits       REAL         NOT NULL,
+    UNIQUE (day, symbol)
 );
 """
 
@@ -31,3 +33,11 @@ def db_stock_setup(uri: str) -> None:
             curr.execute(CREATE_STOCKS_TABLE)
         conn.commit()
     __stocks_uri = uri
+
+
+def insert_or_do_nothing_on_conflict(table, conn, keys, data_iter):
+    insert_stmt = insert(table.table).values(list(data_iter))
+    on_duplicate_key_stmt = insert_stmt.on_conflict_do_nothing(
+        index_elements=["day", "symbol"]
+    )
+    conn.execute(on_duplicate_key_stmt)
